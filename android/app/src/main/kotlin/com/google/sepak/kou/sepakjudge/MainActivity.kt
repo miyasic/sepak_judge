c70d8c5760ac02feda6acf81dda12d5bba7ed941
Kou Miyaji <kou.sepak@gmail.com>
2020-07-04 17:55:58 +0900

    second commit

---
+++ android/app/src/main/kotlin/com/google/sepak/kou/sepakjudge/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.google.sepak.kou.sepakjudge
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
