f5844d16d497750d31d0d511b5cf45b8138a9253
miyaji555 <kou.sepak@gmail.com>
2021-05-05 19:15:39 +0900

    大会一覧ページの体裁を整えました。

--- lib/presentation/entry_competition/entry_competition_page.dart
+++ lib/presentation/entry_competition/entry_competition_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sepakjudge/constants.dart';
 import 'entry_competition_model.dart';
+import 'package:sepakjudge/date_extensions.dart';
 
 class EntryCompetitionPage extends StatelessWidget {
   @override
@@ -14,7 +16,49 @@ class EntryCompetitionPage extends StatelessWidget {
         ),
         body: Consumer<EntryCompetitionModel>(
           builder: (context, model, child) {
-            return Container();
+            return Container(
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                child: ListView.builder(
+                  padding: EdgeInsets.all(3),
+                  itemCount:
+                      model.competitions.length, //inputFileNamesは初期値に空白が入っている。
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ListTile(
+                        tileColor: themeSecondBackColor,
+                        leading: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '${model.competitions[index].name}',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            Text('${model.competitions[index].division}'),
+                          ],
+                        ), //in
+                        trailing: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('エントリー締め切り',
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            Text(model.competitions[index].entryDeadline
+                                .toYearMonthDateDOWString()),
+                          ],
+                        ),
+                        shape: Border(), // putFileNamesは初期値に空白が入っている。
+                        onTap: () async {
+                          //todo:画面遷移・ファイルからデータ取得
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
+            );
           },
         ),
       ),
