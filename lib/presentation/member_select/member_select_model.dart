361d33e9ef4058afe07025bb4879caed0ef35d01
miyaji555 <kou.sepak@gmail.com>
2021-05-06 19:42:32 +0900

    団体の選手を取得して一覧表示できました。

--- lib/presentation/member_select/member_select_model.dart
+++ lib/presentation/member_select/member_select_model.dart
@@ -1,3 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:sepakjudge/domain/player.dart';
+import 'package:sepakjudge/repository/player_repository.dart';
+import 'package:sepakjudge/repository/teams_repository.dart';
+import 'package:sepakjudge/utils/dialog_utils.dart';
 
-class MemberSelectModel extends ChangeNotifier {}
+class MemberSelectModel extends ChangeNotifier {
+  final _teamsRepository = TeamsRepository.instance;
+  List<Player> members;
+
+  Future init(context) async {
+    try {
+      //レグ情報
+      final _currentUser = await PlayersRepository.instance.fetch();
+      members = await _teamsRepository.fetchMembers(_currentUser.teamId);
+      print(members);
+    } catch (e) {
+      DialogUtils.showSimpleDialog(text: e.toString(), context: context);
+    }
+    notifyListeners();
+  }
+}
